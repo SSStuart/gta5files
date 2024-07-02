92e42e7ceb0ef23683b220e59b0f44c4595aafbd
SSStuart <71145823+SSStuart@users.noreply.github.com>
2024-07-02 11:50:08 +0200

    Updated Listing Script + Files list
    - Updated the function arguments so symlink are followed
    - Updated files list for RGL and EGS 1.0.3258.0 versions

--- Tool/ListFiles.py
+++ Tool/ListFiles.py
@@ -2,7 +2,7 @@ content = ""
 f = open("GTAfiles.txt", "w")
 import os
 racine = input("Emplacement racine (X:/.../GTA5) :")
-for root, dirs, files in os.walk(racine):
+for root, dirs, files in os.walk(racine, True, None, True):
      for file in files:
          content += root+"\\"+file+"\n"
          print(root+"\\"+file)
