f855bbba8706c21f9ec59f983ebccdd9c70cc724
SSStuart <71145823+SSStuart@users.noreply.github.com>
2025-04-10 13:55:53 +0200

    GTA 5 Updates (Legacy: v1.0.3504.0 - Enhanced: v1.0.813.11) Updated RGL files for both versions

--- Tool/ListFiles.py
+++ Tool/ListFiles.py
@@ -1,7 +1,7 @@
 content = ""
-f = open("GTAfiles.txt", "w")
 import os
 racine = input("Emplacement racine (X:/.../GTA5) :")
+f = open("GTAfiles.txt", "w")
 for root, dirs, files in os.walk(racine, True, None, True):
      for file in files:
          content += root+"\\"+file+"\n"
